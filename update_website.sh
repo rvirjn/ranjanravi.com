f7b124e67de236aca8e3ccc2a1bcefe686cd6a37
Ravi Ranjan <raviranjan.amu@gmail.com>
2018-12-07 13:04:58 +0530

    Update update_website.sh
    Updated new IP address

--- update_website.sh
+++ update_website.sh
@@ -2,8 +2,8 @@
 echo 'Performing post action for updating raviranjan.org.in'
 chmod 600 key.pem
 mv .git ../
-scp -r -i key.pem . ubuntu@13.233.12.206:/home/ubuntu/html/
-echo 'Project copied to aws vm 13.233.12.206'
-ssh -i key.pem ubuntu@13.233.12.206 'docker cp /home/ubuntu/html/ raviranjan.org.80.80:/usr/share/nginx/'
+scp -r -i key.pem . ubuntu@13.127.120.189:/home/ubuntu/html/
+echo 'Project copied to aws vm 13.127.120.189'
+ssh -i key.pem ubuntu@13.127.120.189 'docker cp /home/ubuntu/html/ ranjanravi.com:/usr/share/nginx/'
 echo 'Project copied to aws docker location /usr/share/nginx/html/'
 mv ../.git .
