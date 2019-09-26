7b1ed438ce85ed50a63fea79f3fffa35f6c0c7fd
Ravi Ranjan <raviranjan@vmware.com>
2019-09-26 23:54:34 +0530

    update all project dockers

--- update_website.sh
+++ update_website.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-echo 'post run after upload_to_production.py'
+echo 'Run upload_to_production.py first'
+#python upload_to_production.py
 echo 'Performing post action for updating ranjanravi.com'
 ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/shopping/. nginx:/usr/share/nginx/html/'
 ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/booking/. tomcat:/usr/local/tomcat/webapps/ROOT/'
 ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/profile/. httpd:/usr/local/apache2/htdocs/'
+
+ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/checkspell/. checkspell:/usr/share/nginx/html/'
+ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/hierarchy/. hierarchy:/usr/local/tomcat/webapps/ROOT/'
+ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/onlinetest/. onlinetest:/usr/local/apache2/htdocs/'
+ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/social/. social:/usr/share/nginx/html/'
+
 echo 'Project copied to docker'
