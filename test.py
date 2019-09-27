c104f6558cf9b3aed8ac03b37fe7f4180535de76
Ravi Ranjan <raviranjan@vmware.com>
2019-09-27 15:52:32 +0530

    add test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+import webbrowser
+try:
+    webbrowser.open_new_tab('https://ranjanravi.com')
+    webbrowser.open_new_tab('http://profile.ranjanravi.com')
+    webbrowser.open_new_tab('http://shopping.ranjanravi.com')
+    webbrowser.open_new_tab('http://booking.ranjanravi.com')
+    webbrowser.open_new_tab('http://checkspell.ranjanravi.com')
+    webbrowser.open_new_tab('http://hierarchy.ranjanravi.com')
+    webbrowser.open_new_tab('http://onlinetest.ranjanravi.com')
+    webbrowser.open_new_tab('http://social.ranjanravi.com')
+
+    webbrowser.open_new_tab('http://13.234.239.179:2222')
+    webbrowser.open_new_tab('http://13.234.239.179:3333')
+    webbrowser.open_new_tab('http://13.234.239.179:4444')
+    webbrowser.open_new_tab('http://13.234.239.179:5555')
+    webbrowser.open_new_tab('http://13.234.239.179:7777')
+    webbrowser.open_new_tab('http://13.234.239.179:8888')
+    webbrowser.open_new_tab('http://13.234.239.179:9999')
+
+except Exception as e:
+    print(e)
