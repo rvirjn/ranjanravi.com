735b2e6b4d22b6bc4bdd25759a0bef55227d8836
Ravi Ranjan <raviranjan@vmware.com>
2019-09-27 14:31:35 +0530

    update docker name

--- update_website.sh
+++ update_website.sh
@@ -2,9 +2,9 @@
 echo 'Run upload_to_production.py first'
 #python upload_to_production.py
 echo 'Performing post action for updating ranjanravi.com'
-ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/shopping/. nginx:/usr/share/nginx/html/'
+ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/shopping/. shopping:/usr/share/nginx/html/'
 ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/booking/. tomcat:/usr/local/tomcat/webapps/ROOT/'
-ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/profile/. httpd:/usr/local/apache2/htdocs/'
+ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/profile/. profile:/usr/share/nginx/html/'
 
 ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/checkspell/. checkspell:/usr/share/nginx/html/'
 ssh -i aws-new-key-sg-new.pem ubuntu@13.234.239.179 'sudo docker cp /home/ubuntu/ranjanravi.com/hierarchy/. hierarchy:/usr/local/tomcat/webapps/ROOT/'
