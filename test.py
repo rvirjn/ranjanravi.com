00992d791477b462f40ef7bfabda589d2ba3cd36
Ravi Ranjan <raviranjan@vmware.com>
2019-09-28 13:52:16 +0530

    update metdata

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import webbrowser
 try:
-    webbrowser.open_new_tab('https://ranjanravi.com')
-    webbrowser.open_new_tab('http://profile.ranjanravi.com')
+    # webbrowser.open_new_tab('https://ranjanravi.com')
+    webbrowser.open_new_tab('https://profile.ranjanravi.com')
     webbrowser.open_new_tab('http://shopping.ranjanravi.com')
     webbrowser.open_new_tab('http://booking.ranjanravi.com')
     webbrowser.open_new_tab('http://checkspell.ranjanravi.com')
@@ -13,7 +13,7 @@ try:
     webbrowser.open_new_tab('http://13.234.239.179:3333')
     webbrowser.open_new_tab('http://13.234.239.179:4444')
     webbrowser.open_new_tab('http://13.234.239.179:5555')
-    webbrowser.open_new_tab('http://13.234.239.179:7777')
+    webbrowser.open_new_tab('https://13.234.239.179:7777')
     webbrowser.open_new_tab('http://13.234.239.179:8888')
     webbrowser.open_new_tab('http://13.234.239.179:9999')
 
