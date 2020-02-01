e9d5b52ce00bd528cca4a3524d68359065a97285
Ravi Ranjan <raviranjan@vmware.com>
2020-02-01 12:48:31 +0530

    add farmination.com content

--- update_website.sh
+++ update_website.sh
@@ -8,13 +8,16 @@ if [ $# -eq 1 ]
 echo 'Running upload_to_production.py first'
 # python upload_to_production.py
 echo 'Performing post action for updating ranjanravi.com'
+ssh -i aws-new-key-sg-new.pem ubuntu@13.127.99.82 'sudo docker cp /home/ubuntu/ranjanravi.com/farmination/. checkspell:/usr/local/apache2/htdocs/'
+# ssh -i aws-new-key-sg-new.pem ubuntu@13.127.99.82 'sudo docker cp /home/ubuntu/ranjanravi.com/checkspell/. checkspell:/usr/local/apache2/htdocs/'
+
+# ssh -i aws-new-key-sg-new.pem ubuntu@13.127.99.82 'sudo docker cp /home/ubuntu/ranjanravi.com/hierarchy/. hierarchy:/usr/local/tomcat/webapps/ROOT/'
+ssh -i aws-new-key-sg-new.pem ubuntu@13.127.99.82 'sudo docker cp /home/ubuntu/ranjanravi.com/school/. hierarchy:/usr/local/tomcat/webapps/ROOT/'
+
 ssh -i aws-new-key-sg-new.pem ubuntu@13.127.99.82 'sudo docker cp /home/ubuntu/ranjanravi.com/shopping/. shopping:/usr/share/nginx/html/'
 ssh -i aws-new-key-sg-new.pem ubuntu@13.127.99.82 'sudo docker cp /home/ubuntu/ranjanravi.com/booking/. booking:/usr/local/tomcat/webapps/ROOT/'
 ssh -i aws-new-key-sg-new.pem ubuntu@13.127.99.82 'sudo docker cp /home/ubuntu/ranjanravi.com/profile/. profile:/usr/share/nginx/html/'
 
-ssh -i aws-new-key-sg-new.pem ubuntu@13.127.99.82 'sudo docker cp /home/ubuntu/ranjanravi.com/checkspell/. checkspell:/usr/local/apache2/htdocs/'
-# ssh -i aws-new-key-sg-new.pem ubuntu@13.127.99.82 'sudo docker cp /home/ubuntu/ranjanravi.com/hierarchy/. hierarchy:/usr/local/tomcat/webapps/ROOT/'
-ssh -i aws-new-key-sg-new.pem ubuntu@13.127.99.82 'sudo docker cp /home/ubuntu/ranjanravi.com/school/. hierarchy:/usr/local/tomcat/webapps/ROOT/'
 ssh -i aws-new-key-sg-new.pem ubuntu@13.127.99.82 'sudo docker cp /home/ubuntu/ranjanravi.com/onlinetest/. onlinetest:/usr/local/apache2/htdocs/'
 ssh -i aws-new-key-sg-new.pem ubuntu@13.127.99.82 'sudo docker cp /home/ubuntu/ranjanravi.com/social/. social:/usr/share/nginx/html/'
 
